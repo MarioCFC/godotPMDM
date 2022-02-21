2ad1eec26fcb72f6048eeb873b35ff4002484ab7
Mario Cascudo <mariocscdclase@gmail.com>
2022-02-21 14:52:43 +0100

    Inicio del repositorio

---
+++ Scripts/Jugador.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+var posicion = Vector2()
+var velocidad = 150
+var potenciaSalto = -150
+var gravedad = 5
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	input(delta)
+	animacion()
+	
+	posicion.y += gravedad
+	posicion = move_and_slide(posicion,Vector2.UP)
+	
+func input(delta):
+	var  direccionHorizontal = (int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")))
+	
+	if(Input.is_action_just_pressed("ui_up") && is_on_floor()):
+		posicion.y = potenciaSalto 
+		
+	posicion.x = direccionHorizontal * velocidad
+
+func animacion():
+	if(posicion.x != 0 && is_on_floor()):
+		$AnimatedSprite.play("Run")
+	elif(is_on_floor()):
+		$AnimatedSprite.play("Idle")
+	elif(!is_on_floor() && posicion.y < 0):
+		$AnimatedSprite.play("Jump_Up")
+	elif(!is_on_floor()):
+		$AnimatedSprite.play("Jump_Down")
+	if(posicion.x != 0):
+		$AnimatedSprite.flip_h = posicion.x < 0
+
+
+#Tambien afecta a los TileMaps con collisionShape por lo que hay
+#que cambiar las máscaras de colision
+func _on_Area2D_body_entered(body):
+	print("Respawn")
+	self.global_position = Vector2(98,36)
